a92a5d539719443ab1defa247d14827575e129c7
Alexander Sharov <kvendingoldo@yandex.ru>
2018-03-17 03:21:57 +0400

    Improve style

--- src/website/settings.py
+++ src/website/settings.py
@@ -29,7 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website.news'
+    'website.news',
+    'material',
+    'material.frontend',
 ]
 
 MIDDLEWARE = [
